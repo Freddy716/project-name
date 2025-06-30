3a14758535ddd6adeffbbd04d58c911eb9417e9c
Freddy Flores <fredyubi@gmail.com>
2025-06-30 18:33:41 -0400

    add usuarios crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EstadosModule } from './feature/estados/estados.module';
+import { UsuariosModule } from './feature/usuarios/usuarios.module';
 
 @Module({
-  imports: [EstadosModule],
+  imports: [EstadosModule, UsuariosModule],
   controllers: [AppController],
   providers: [AppService],
 })
